c15a1676455d7cd4297effc1bfffe64c965e5a14
Mohamed Farahat <Muhammad.frahat00@gmail.com>
2025-06-27 21:34:17 +0300

    Update DBT_DAG.py

--- DBT_DAG.py
+++ DBT_DAG.py
@@ -2,36 +2,29 @@ from airflow import DAG
 from airflow.operators.bash import BashOperator
 from datetime import datetime, timedelta
 
-
 # DBT PATH
-DBT_PROJECT_DIR = "C:\Users\mofar\dbt-project" 
+DBT_PROJECT_DIR = r"C:\Users\mofar\dbt-project"  # raw string to avoid escape issues
 
 # DAG Definition 
+with DAG(
+    dag_id="dbt_dag",
+    start_date=datetime(2025, 6, 28),
+    schedule_interval="@daily",  # fixed typo
+    catchup=False,
+    tags=["snowflake", "daily", "dbt"],
+) as dag:
 
-with DAG( dag_id= "dbt_dag",
-         start_date= datetime(2025,6,28),
-         scedule_interval= "@daily",
-         catchup=False,
-         tags= ["snowflake","daily","dbt"],
-) as dag :
-  
-# first task
-  dbt_run = BashOperator(
-    task_id='dbt_run',
-    bash_command=f'cd {DBT_PROJECT_DIR} && dbt run',
-)
-# second task
-dbt_test = BashOperator(
-    task_id='dbt_test',
-    bash_command=f'cd {DBT_PROJECT_DIR} && dbt test',
-)
+    # first task
+    dbt_run = BashOperator(
+        task_id='dbt_run',
+        bash_command=f'cd {DBT_PROJECT_DIR} && dbt run',
+    )
 
-# Define task dependencies
-dbt_run >> dbt_test  
+    # second task
+    dbt_test = BashOperator(
+        task_id='dbt_test',
+        bash_command=f'cd {DBT_PROJECT_DIR} && dbt test',
+    )
 
-
-  
-
-
-
-  
+    # Define task dependencies
+    dbt_run >> dbt_test
