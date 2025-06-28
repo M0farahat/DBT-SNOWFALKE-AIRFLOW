82327fbfb2d02d408407d04dce3e0d8215395c32
Mohamed Farahat <Muhammad.frahat00@gmail.com>
2025-06-28 15:59:11 +0300

    Update DBT_DAG.py

--- DBT_DAG.py
+++ DBT_DAG.py
@@ -3,7 +3,7 @@ from airflow.operators.bash import BashOperator
 from datetime import datetime, timedelta
 
 # DBT PATH
-DBT_PROJECT_DIR = r"C:\Users\mofar\dbt-project"  # raw string to avoid escape issues
+DBT_PROJECT_DIR = "/usr/local/airflow/include/dbt_SF"  
 
 # DAG Definition 
 with DAG(
@@ -28,3 +28,37 @@ with DAG(
 
     # Define task dependencies
     dbt_run >> dbt_test
+
+
+#DAG with decorator
+
+
+from airflow.decorators import dag
+from airflow.operators.bash import BashOperator
+from datetime import datetime
+
+DBT_PROJECT_DIR = "/usr/local/airflow/include/dbt_SF"
+
+
+@dag(
+    dag_id="dbt_daggll",
+    start_date=datetime(2025, 6, 28),
+    schedule="@daily",
+    catchup=False,
+    tags=["dbt", "snowflake"],
+)
+def dbt_workflow():
+
+    dbt_run = BashOperator(
+        task_id='dbt_run',
+        bash_command=f'cd {DBT_PROJECT_DIR} && dbt run 
+
+    dbt_test = BashOperator(
+        task_id='dbt_test',
+        bash_command=f'cd {DBT_PROJECT_DIR} && dbt test 
+    )
+
+    dbt_run >> dbt_test
+
+
+dbt_workflow()
